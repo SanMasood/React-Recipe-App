b04c82756d9e8457e61edaabf696ee1bceaf8690
SanMasood <sanamasood.giki@gmail.com>
2020-06-22 17:33:22 +0100

    Project setup & key setup from Edamam

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () =>
+
+ {
+   const APP_ID = "9fae0fea";
+   const APP_KEY = "6f0072a70705253a67f84f750992b1b4";
+   return(
+     <div className="App">
+       <h1>Hello React</h1>
+     </div>
+   );
+ }
+
+export default App;
